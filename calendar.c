0e93597e64b914df1e57d6a488b01f1bffbbd8bc
Abdullah Al Jehan <abdullahaljehan.me@gmail.com>
2026-09-04 23:52:58 +0600

    Add calendar functionality for displaying months
    Implement a calendar program that displays the month's days based on user input for the year, accounting for leap years.

---
+++ calendar.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+// FINDING THE FIRST DAY OF A YEAR USING A GREGRIAN CALENDER FORMULA
+
+int getFirstDayOfTheYear(int year)
+{
+    int day = (year * 365 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400) % 7;
+}
+int main(int argc, char const *argv[])
+{
+    char *month[] = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    for (int i = 0; i < 12; i++)
+    {
+        printf("\n\n==============================================================\n\t\t\t%s\n==============================================================\n\n", month[i]);
+        printf("\tSUN\tMON\tTUE\tWED\tTHU\tFRI\tSAT\n");
+        int weekDay = 0, spaceCount = 0, year;
+
+        printf("ENTER YOUR WANTED YEAR :  ");
+        scanf("%d", &year);
+
+        printf("======================= %d CALENDAR =======================");
+
+        // CHECKING IF IT IS LEAP YEAR OR NOT
+
+        if (year % 4 == 0 && year % 100 != 0 || (year % 400 == 0))
+        {
+            daysInMonth[1] = 29;
+        }
+
+        for (spaceCount = 0; spaceCount <= weekDay; spaceCount++)
+        {
+            printf("\t\t\t\t\t");
+        }
+
+        int totalDays = daysInMonth[i];
+        for (int d = 1; d <= totalDays; d++)
+        {
+            printf("\t%d", d);
+            weekDay++;
+            if (weekDay > 6)
+            {
+                weekDay = 0;
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
